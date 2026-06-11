8034ecf7826e884dd46c3b5f19aa2bb609d9af8d
phananh2005 <phantran14122005@gmail.com>
2026-06-12 00:21:45 +0700

    done resposive

--- src/components/item/Deltail.tsx
+++ src/components/item/Deltail.tsx
@@ -25,7 +25,7 @@ export default function Deltail({
   const { addToCart } = useCart();
 
   return (
-    <div className="top-48.25 left-192.25 w-126.75 h-127.75 flex flex-col justify-between pt-8 px-8">
+    <div className="flex flex-col w-full h-full justify-between items-center pt-8 px-8 gap-4">
       <div className="flex flex-col w-full h-fit gap-5.5">
         <div className="w-110.75 h-9 font-['Work_Sans'] font-light text-[36px] leading-9">
           {title}
@@ -35,23 +35,35 @@ export default function Deltail({
         </div>
       </div>
 
-      <div className="flex flex-col w-full h-40 gap-2">
+      <div className="flex flex-col w-full h-fit gap-2">
         <div className="flex flex-col w-full h-fit gap-8">
           <div className="flex items-center gap-3">
-            <span className="font-['Work_Sans'] font-normal text-base">
+            <span
+              className="font-['Work_Sans'] font-normal text-base
+            md:text-[27px]"
+            >
               Materials:
             </span>
-            <span className="font-['Work_Sans'] font-light text-base">
+            <span
+              className="font-['Work_Sans'] font-light text-base
+            md:text-[27px]"
+            >
               {materials}
             </span>
           </div>
-          <div className="flex w-fit h-fit gap-4.5 font-['Work_Sans'] font-normal text-base">
+          <div
+            className="flex w-fit h-fit gap-4.5 font-['Work_Sans'] font-normal text-base
+            md:text-[27px]"
+          >
             Description:
           </div>
         </div>
 
         <div className="flex flex-col w-full h-full gap-2.5">
-          <p className="font-['Work_Sans'] font-light text-sm leading-6">
+          <p
+            className="font-['Work_Sans'] font-light text-sm leading-6
+            md:text-[27px] md:leading-9"
+          >
             {description}
           </p>
         </div>
@@ -59,12 +71,18 @@ export default function Deltail({
 
       <div className="flex flex-col w-full h-fit gap-8">
         <div className="flex flex-col w-27.5 h-fit">
-          <div className="font-['Work_Sans'] font-light text-base">Size</div>
+          <div
+            className="font-['Work_Sans'] font-light text-base
+            md:text-[27px]"
+          >
+            Size
+          </div>
           <div className="flex w-full h-fit gap-2">
             <Size
               sizes={["S", "M", "L"]}
               selectedSize={selectedSize}
               onSelectSize={(s) => setSelectedSize(s)}
+              className="md:text-[27px] w-[40px] h-[40px]"
             />
           </div>
         </div>
@@ -83,7 +101,8 @@ export default function Deltail({
               quantity: 1,
             })
           }
-          className="flex w-full h-10 gap-1 bg-[#000000] border: 1px solid #FFFFFF text-white justify-center items-center"
+          className="flex w-full h-10 gap-1 bg-[#000000] border: 1px solid #FFFFFF text-white justify-center items-center
+          md:h-12 md:gap-2 md:text-[27px]"
         >
           Add to cart
         </button>
@@ -94,7 +113,7 @@ export default function Deltail({
           aria-pressed={favorite}
           className="h-9 w-9 flex items-center justify-center"
         >
-          <HeartShape state={favorite} />
+          <HeartShape state={favorite} className="md:h-[150px] md:w-[150px]" />
         </button>
       </div>
     </div>
